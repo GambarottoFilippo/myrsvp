69746380a56d6a12902e99abd94a366183793b00
GambarottoFilippo <gambarotto.filippo@gmail.com>
2019-04-13 14:33:02 +0200

    My first commit

---
+++ _site/assets/js/swiper.js
@@ -0,0 +1,34 @@
+new Swiper ('.swiper-container-1', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-1',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next-1',
+    prevEl: '.swiper-button-prev-1',
+  }
+})
+
+new Swiper ('.swiper-container-2', {
+  navigation: {
+    nextEl: '.swiper-button-next-2',
+    prevEl: '.swiper-button-prev-2',
+  }
+})
+
+new Swiper ('.swiper-container-3', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-3',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next-3',
+    prevEl: '.swiper-button-prev-3',
+  }
+})
