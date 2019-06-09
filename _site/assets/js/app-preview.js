38f0a7a948bbcdaa261151065518b7e02bb065be
GambarottoFilippo <gambarotto.filippo@gmail.com>
2019-06-09 13:46:54 +0200

    Fix contacts and app-preview

--- _site/assets/js/app-preview.js
+++ _site/assets/js/app-preview.js
@@ -17,3 +17,33 @@ $('#refuse_submit').on('click', function() {
     $('#modal_confirm').modal('hide');
   }, 3000);
 })
+
+$("[data-name='preferences_count_1']").on('click', function(e) {
+  var old_value = parseInt($(this).children('div').html());
+  if ($(e.target).hasClass('minus-btn') && old_value > 1) {
+    $(this).children('div').html(old_value - 1);
+    if (old_value === 2) {
+      $(this).children('.minus-btn').attr('src', 'assets/img/app-preview/minus.svg')
+    }
+  } else if ($(e.target).hasClass('plus-btn')) {
+    $(this).children('div').html(old_value + 1);
+    if (old_value === 1) {
+      $(this).children('.minus-btn').attr('src', 'assets/img/app-preview/minus-active.svg')
+    }
+  }
+})
+
+$("[data-name='preferences_count_0']").on('click', function(e) {
+  var old_value = parseInt($(this).children('div').html());
+  if ($(e.target).hasClass('minus-btn') && old_value > 0) {
+    $(this).children('div').html(old_value - 1);
+    if (old_value === 1) {
+      $(this).children('.minus-btn').attr('src', 'assets/img/app-preview/minus.svg')
+    }
+  } else if ($(e.target).hasClass('plus-btn')) {
+    $(this).children('div').html(old_value + 1);
+    if (old_value === 0) {
+      $(this).children('.minus-btn').attr('src', 'assets/img/app-preview/minus-active.svg')
+    }
+  }
+})
